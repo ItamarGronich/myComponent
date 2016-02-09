871625808044fd4ee1f56c7a2d13ce0323e986fb
ItamarGronich <ItamarGronich@Gmail.com>
2016-02-09 19:45:25 +0200

    first commit

---
+++ index-compiled.js
@@ -0,0 +1,11 @@
+var _ = require('underscore');
+
+function html() {
+    console.log(_);
+}
+
+module.exports = {
+    html: html
+};
+
+//# sourceMappingURL=index-compiled.js.map
---
+++ index.js
@@ -0,0 +1,10 @@
+var _ = require('underscore');
+
+
+function html() {
+    console.log(_)
+}
+
+module.exports  = {
+    html: html
+};
